25483bde556508ee062b4303a6082c218890446b
Giovana Thomé <99211056+ThomeGiovana@users.noreply.github.com>
2023-02-09 10:42:43 -0300

    semana2

---
+++ instrucoesProgramacao/arraysAula/DiscreteDistribution.java
@@ -0,0 +1,15 @@
+public class DiscreteDistribution {
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int[] sum = new int[args.length];
+        sum[0] = 0;
+
+        for (int i = 1; i < args.length; i++) {
+            sum[i] = Integer.parseInt(args[i]) + sum[i - 1];
+        }
+
+        for (int i = 1; i <= m; i++) {
+            double r = (Math.random());
+        }
+    }
+}
